d42a8389cc67a92eceb912294cdebe14a031b3ef
eidman <eidmanx@gmail.com>
2020-01-18 12:28:31 -0500

    simplified js + added box delete

--- index.js
+++ index.js
@@ -2,55 +2,37 @@
 //  * document - virtual representation of the entire page
 //  * variable - a way to save a piece of data, so we can reuse it later
 //  * event listener - a way to register events that happen in the browser
-//  * string - piece of text
 //  * element - virtual representation of the html on the page
 // */
 
-// wait for the page to be fully loaded
-
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("grey-btn").addEventListener("click", createGreyBox);
-  function createGreyBox() {
-    var newGreyBox = document.createElement("div");
-    newGreyBox.className = "created-box grey";
-    document.getElementById("box-container").appendChild(newGreyBox);
-    console.log("grey button was pressed");
-  }
-
-  document
-    .getElementById("darkgrey-btn")
-    .addEventListener("click", createDarkgreyBox);
-  function createDarkgreyBox() {
-    var newDarkgreyBox = document.createElement("div");
-    newDarkgreyBox.className = "created-box darkgrey";
-    document.getElementById("box-container").appendChild(newDarkgreyBox);
-    console.log("dark grey button was pressed");
-  }
-
-  document
-    .getElementById("lightgrey-btn")
-    .addEventListener("click", createLightgreyBox);
-  function createLightgreyBox() {
-    var newLightgreyBox = document.createElement("div");
-    newLightgreyBox.className = "created-box lightgrey";
-    document.getElementById("box-container").appendChild(newLightgreyBox);
-    console.log("light grey button was pressed");
-  }
-
-  document
-    .getElementById("silver-btn")
-    .addEventListener("click", createSilverBox);
-  function createSilverBox() {
-    var newSilverBox = document.createElement("div");
-    newSilverBox.className = "created-box silver";
-    document.getElementById("box-container").appendChild(newSilverBox);
-    console.log("silver button was pressed");
-  }
-
-  var SilverBox = document.getElementById("box-container");
-  var newSilverBox = document.createElement("div");
-  newSilverBox.className = "created-box silver";
-  document.getElementById("box-container").appendChild(newSilverBox);
-  SilverBox.removeChild(newSilverBox);
-  console.log("silver button was deleted");
+  // select ALL of the buttons
+  var buttons = document.querySelectorAll(".btn");
+  // loop through each button that you found in your list
+  // then run the code inside of the function
+  buttons.forEach(function(button) {
+    // runs 3 times
+    button.addEventListener("click", function() {
+      var color = button.getAttribute("data-color");
+      var boxContainer = document.querySelector(".box-container");
+      var box = document.createElement("div");
+      box.className = "box " + color;
+      boxContainer.appendChild(box);
+      document.querySelector(".box");
+      box.addEventListener("click", function() {
+        this.remove();
+      });
+    });
+  });
 });
+
+//   document
+//     .getElementById("silver-btn")
+//     .addEventListener("click", createSilverBox);
+//   function createSilverBox() {
+//     var newSilverBox = document.createElement("div");
+//     newSilverBox.className = "created-box silver";
+//     document.getElementById("box-container").appendChild(newSilverBox);
+//     console.log("silver button was pressed");
+//   }
+// });
